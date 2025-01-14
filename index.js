440a5f80484ded505b8c74669dc222d6900ad4a1
Louis GINDRE <louis.gindre@edu.devinci.fr>
2025-01-14 18:30:06 +0100

    Pinata initialization

---
+++ index.js
@@ -0,0 +1,8 @@
+import { PinataSDK } from "pinata-web3";
+
+const pinata = new PinataSDK({
+    pinataJwt: process.env.PINATA_JWT,
+    pinataGateway: process.env.PINATA_GATEWAY,
+});
+
+console.log("Pinata SDK initialized!");
